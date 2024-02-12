38bb33a4138f39375a60fded6b5ae38ed287eadc
Kacper Grabiec <grabiec.kacper01@gmail.com>
2024-02-12 22:36:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# **grabieckacper.com**
+
+A source code of [grabieckacper.com](https://www.grabieckacper.com/) website.
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Kacper Grabiec",
+  description: "Full-stack developer.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={jetBrainsMono.className}>{children}</body>
+    </html>
+  );
+}
