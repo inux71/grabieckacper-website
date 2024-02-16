04c428ac05ae3c3345e87be7a3be37434ddf1285
Kacper Grabiec <grabiec.kacper01@gmail.com>
2024-02-16 16:54:24 +0100

    add experience section

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -1,3 +1,26 @@
+import Work from "../work";
+
 export default function Experience() {
-  return <section id="experience" className="min-h-screen"></section>;
+  return (
+    <section
+      id="experience"
+      className="min-h-screen flex flex-col justify-center items-center"
+    >
+      <Work
+        position="Full-stack developer"
+        company="SkyNow"
+        period={["October 2023", "December 2023"]}
+        description="Editing existing web applications and REST-API interfaces during 3 months long internship."
+        technologies={["Java", "Spring Boot", "JavaScript", "React", "Docker"]}
+      />
+
+      <Work
+        position=".NET developer"
+        company="POL-EKO"
+        period={["July 2023", "September 2023"]}
+        description="Creating web application and REST-API interface for .NET platform during 3 months long internship."
+        technologies={["C#", "ASP.NET Core", "Entity Framework Core", "Blazor"]}
+      />
+    </section>
+  );
 }
---
+++ src/components/work.tsx
@@ -0,0 +1,35 @@
+export default function Work({
+  position,
+  company,
+  period,
+  description,
+  technologies,
+}: {
+  position: string;
+  company: string;
+  period: [string, string];
+  description: string;
+  technologies: string[];
+}) {
+  return (
+    <div className="flex flex-col justify-center items-start w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 m-1 p-1 hover:scale-105 hover:cursor-default">
+      <>
+        <h3 className="text-2xl font-extrabold">{position}</h3>
+        <p className="text-xs text-[#777]">{`${period[0]} - ${period[1]}`}</p>
+        <h5 className="text-lg py-2">{company}</h5>
+      </>
+
+      <p className="italic">{description}</p>
+
+      <div className="flex flex-wrap justify-start items-start pt-2">
+        {technologies.map((technology: string, index: number) => {
+          return (
+            <div key={index} className="flex text-sm bg-[#222] p-1 m-1 rounded">
+              <span>{technology}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
